0bbe55b329dcd431ac990a4936891fb70e39c92d
YonJun <jhon_08_2007@hotmail.com>
2021-05-08 11:56:16 -0500

    update product card

--- src/pages/Home/components/ProductCard.jsx
+++ src/pages/Home/components/ProductCard.jsx
@@ -2,6 +2,7 @@
 // eslint-disable-next-line
 import tw, { styled } from "twin.macro";
 import { Fragment } from "react";
+import { Body } from "components/Typography";
 
 const ImgContainer = styled.div`
   margin-top: -40px;
@@ -10,18 +11,24 @@ const Img = styled.img`
   max-width: 130px;
 `;
 
-const ProductCard = () => {
+const ProductCard = ({ title = "", price = "", qty = 0 }) => {
   return (
     <Fragment>
-      <div tw="p-5 bg-dark-2 rounded-3xl">
+      <div tw="p-5 bg-dark-2 rounded-3xl text-center hover:cursor-pointer">
         <ImgContainer tw="flex justify-center">
           <Img
-            alt="plate of food"
+            alt="Bowl of food"
             tw="w-full h-auto block"
             src="https://www.pngkit.com/png/full/54-545849_dinner-food-png-freeuse-stock-plate-of-food.png"
           />
         </ImgContainer>
-        <div tw="">ProductCard</div>
+        <div tw="pt-5">
+          <Body gutterBottom>{title}</Body>
+          <Body gutterBottom>$ {price}</Body>
+          <Body gutterBottom tw="text-tlight">
+            {qty} Bowls available
+          </Body>
+        </div>
       </div>
     </Fragment>
   );
--- src/pages/Home/layouts/ProductList.jsx
+++ src/pages/Home/layouts/ProductList.jsx
@@ -8,8 +8,13 @@ const ProductList = () => {
   return (
     <Fragment>
       <div tw="w-full grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-10">
-        {[...new Array(10).keys()].map((i) => (
-          <ProductCard key={i} />
+        {[...new Array(9).keys()].map((i) => (
+          <ProductCard
+            key={i}
+            title={"Spicy seasoned seafood noodles"}
+            price={"2.29"}
+            qty={20}
+          />
         ))}
       </div>
     </Fragment>
